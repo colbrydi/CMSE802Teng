97f756788d9c73dad65e9103ca88e37a54809f7e
stormocean <zhangteng_ocean@163.com>
2020-02-18 00:41:26 -0500

    Add files via upload

---
+++ CMSE802Teng/funcs.py
@@ -0,0 +1,42 @@
+import os
+import numpy as np
+
+class PanSettlement():
+    '''
+    the object is a image, which is about to be divisioned into finer grids or settlements (as the next class)
+    '''
+
+    def __init__ ():
+        '''
+        basic attributes of this image
+        '''
+        self.size = 100*100
+        
+        return
+    
+    def division (self, size ):
+        '''
+        the function could divide the images into different pixels, or settlements..
+        '''
+        return
+class settlement():
+    '''
+    each human settlement is a object in this case.
+    
+    '''
+
+    def __init__ ():
+        '''
+        basic attribute of each settlements
+        '''
+        self.pop61
+        self.pop71
+        self.pop80
+        self.poploss
+        
+    def rltloc ():
+        '''
+        basic function detact its spaital relation with the other settlements surrounding
+        '''
+        return
+    
